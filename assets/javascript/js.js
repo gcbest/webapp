2010f375c05aeb691f73552e51cde91f95f982f9
Nate Shih <nshih.85@gmail.com>
2016-09-22 20:50:32 -0400

    Modal testing

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -7,3 +7,10 @@
     messagingSenderId: "961593911482"
   };
   firebase.initializeApp(config);
+
+  // Document Ready with the modal trigger
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+          
