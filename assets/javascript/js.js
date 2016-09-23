8a61166ff7b3e6820e04c069d8a883c924e288c7
Nate Shih <nshih.85@gmail.com>
2016-09-22 21:15:16 -0400

    Jquery moved

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -11,7 +11,20 @@
   // Document Ready with the modal trigger
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+      ready: function() { alert('Ready'); }, // Callback for Modal open
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+      
+
+
   });
           
   // =========Start Yelp API=========
@@ -115,10 +128,10 @@ function restaurantFinder() {
 };
 // =========End Yelp API=========
 
-// =========Start Google Maps API ======
-function locationFinder{
-	var = auth {"https://maps.googleapis.com/maps/api/geocode/json?address=" +1600+Amphitheatre+Parkway,+Mountain+View,+CA+"&key=AIzaSyCNEH9ddgTnDDO-HPKQtW1INRnXiYkp5aA"};
-} // close locationFinder function
+// // =========Start Google Maps API ======
+// function locationFinder( {
+// 	var = auth {"https://maps.googleapis.com/maps/api/geocode/json?address=" +1600+Amphitheatre+Parkway,+Mountain+View,+CA+"&key=AIzaSyCNEH9ddgTnDDO-HPKQtW1INRnXiYkp5aA"};
+// }); // close locationFinder function
 
 
 
