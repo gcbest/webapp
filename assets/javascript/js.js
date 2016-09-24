f2dcb41ba14eeeaddbcde538abd7ba83566e38ee
Juanita Soranno <juanitasoranno@gmail.com>
2016-09-24 10:28:10 -0400

    Adding some Google

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -116,9 +116,39 @@ function restaurantFinder() {
 // =========End Yelp API=========
 
 // =========Start Google Maps API ======
-function locationFinder{
-	var = auth {"https://maps.googleapis.com/maps/api/geocode/json?address=" +1600+Amphitheatre+Parkway,+Mountain+View,+CA+"&key=AIzaSyCNEH9ddgTnDDO-HPKQtW1INRnXiYkp5aA"};
-} // close locationFinder function
+//function locationFinder (){
+  //set variables
+	//var auth = "https://maps.googleapis.com/maps/api/geocode/json?address=" +1600+Amphitheatre+Parkway,+Mountain+View,+CA+"&key=AIzaSyCNEH9ddgTnDDO-HPKQtW1INRnXiYkp5aA";
+
+  var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+
+  function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+
+//}; // close locationFinder function
 
 
 
